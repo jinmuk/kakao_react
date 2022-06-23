2f4b871682838a7eb41de31cd6c7089610012c17
spaper <spaper@naver.com>
2022-06-24 00:04:46 +0900

    add javascript access

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,41 @@
-import React from 'react'
-import axios from 'axios';
+import React, {useState, useEffect} from 'react'
+// import axios from 'axios';
 
 function Main() {
   
-    const getProfile =  () => {
-        let data =  axios.get('https://kapi.kakao.com/v2/user/me');
-        console.log(data);
+    // const getProfile =  () => {
+    //     let data =  axios.get('https://kapi.kakao.com/v2/user/me');
+    //     console.log(data);
+    // }
+    const [token, setToken] = useState();
+
+    useEffect(() => {
+      setToken( window.Kakao.Auth.getAccessToken() );
+    },[]);
+
+    function handleLogin(){
+      window.Kakao.Auth.login({
+        success: function(res){
+          console.log(res);
+          setToken(res.access_token);
+          // window.Kakao.API.re
+        }
+      })
+    }
+    function handleGetUserInfo(){
+      window.Kakao.API.request({
+        url : '/v2/user/me',
+        success: function(res){
+          console.log(res);
+        }
+      })
     }
 
   return (
     <div>main
-        <button onClick={getProfile}>main profile</button>
+        <button onClick={handleLogin}>main Login</button>
+        <button onClick={handleGetUserInfo}>get UserInfo</button>
+        <div>token: {token}</div>
     </div>
   )
 }
